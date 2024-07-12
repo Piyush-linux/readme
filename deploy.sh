9ba247d49903e61df92d911e7ba33d4a3b1fdae4
Piyush-linux <piyushpatil98@protonmail.com>
2024-07-12 21:30:14 +0530

    Git init

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh 
+
+set -e # # abort on errors
+retype build # build
+cd .retype/  # navigate into the build output directory
+
+git init 
+git add -A
+git commit -m 'deploy' # if you are deploying to https://<USERNAME>.github.io 
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+
+git push -f git@github.com:Piyush-linux/readme.git master:gh-pages 
+
---
+++ readme.md
@@ -0,0 +1,23 @@
+# R E A D M E
+
+---
+
+## Setup
+```sh
+sudo npm install retypeapp --global
+git clone <repo>
+cd <repo>
+retype start
+retype build
+```
+
+## Sitemap
+- Frontend
+	- UI : Template
+	- Assets: icon, illustration
+	- Tailwind
+- Backend
+- Database
+- Server
+	- Linux
+
